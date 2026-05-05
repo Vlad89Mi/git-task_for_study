6a8163e3f6ed1fd99b22a7a1bb5ebc154b2b7fa5
Владимир Михалев <waha_vov4ik@mail.ru>
2026-05-06 01:16:41 +0300

    fix bugs and add sqrt+

--- calc.py
+++ calc.py
@@ -19,6 +19,7 @@ def sqrt(x):
         raise ValueError("Квадратный корень из отрицательного числа.")
     return math.sqrt(x)
 
+
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
     print(f"2 + 2 = {add(2, 2)}")
